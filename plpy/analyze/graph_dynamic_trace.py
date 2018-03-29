c3d74c4957a7881914c2088c9f7088a964483583
Jose Cambronero <jcamsan@mit.edu>
2018-03-29 18:24:11 -0400

    fixed plotting issue

--- plpy/analyze/graph_dynamic_trace.py
+++ plpy/analyze/graph_dynamic_trace.py
@@ -3,14 +3,18 @@ import networkx as nx
 
 from .dynamic_trace_events import *
 
+GRAPH_SRC_ATTRIBUTE = 'src'
+
 class TraceToGraph(object):
-    def __init__(self):
+    def __init__(self, include_unknown=True):
         self.counter = 0
+        # note that pydot doesn't like negatives...
+        self.unknown_id = self.allocate_node_id()
         self.graph = nx.DiGraph()
+        self.include_unknown = include_unknown
         self.lineno_to_nodeid = {}
         self.mem_loc_to_lineno = {}
         self.consuming = []
-        self.unknown_id = -1
 
     def allocate_node_id(self):
         _id = self.counter
@@ -23,17 +27,25 @@ class TraceToGraph(object):
 
         node_id = self.allocate_node_id()
         self.graph.add_node(node_id)
-        self.graph.node[node_id]['line'] = event.line
+        self.graph.node[node_id][GRAPH_SRC_ATTRIBUTE] = event.line
         self.lineno_to_nodeid[event.lineno] = node_id
-        dependencies = [(self.get_latest_node_id_for_mem_loc(ml), node_id) for ml in event.uses_mem_locs]
+        dependencies = []
+        # ignore mem locations that are unknown
+        for name, ml in event.uses_mem_locs.items():
+            if self.include_unknown or ml in self.mem_loc_to_lineno:
+                ml_id = self.get_latest_node_id_for_mem_loc(name, ml)
+                dependencies.append((ml_id, node_id))
+
         self.graph.add_edges_from(dependencies)
 
-    def get_latest_node_id_for_mem_loc(self, mem_loc):
+    def get_latest_node_id_for_mem_loc(self, name, mem_loc):
         if not mem_loc in self.mem_loc_to_lineno:
-            # we shoulda llocate a new node here
+            # one of the unknown locations
+            # create new node if needed and accumulate string for debugging when drawn
             if not self.unknown_id in self.graph.nodes:
                 self.graph.add_node(self.unknown_id)
-                self.graph.node[self.unknown_id]['line'] = 'UNKNOWN'
+                self.graph.nodes[self.unknown_id][GRAPH_SRC_ATTRIBUTE] = 'UNKNOWNS: '
+            self.graph.nodes[self.unknown_id][GRAPH_SRC_ATTRIBUTE] += ('%s,' % name)
             return self.unknown_id
         else:
             lineno = self.mem_loc_to_lineno[mem_loc]
@@ -43,7 +55,7 @@ class TraceToGraph(object):
         if self.consuming:
             return
 
-        for mem_loc in event.mem_locs:
+        for name, mem_loc in event.mem_locs.items():
             self.mem_loc_to_lineno[mem_loc] = event.lineno
 
     def handle_EnterCall(self, event):
@@ -67,7 +79,7 @@ class TraceToGraph(object):
 
 def draw(g, dot_layout=True):
     fig, ax = plt.subplots(1)
-    labels = nx.get_node_attributes(g, 'line')
+    labels = nx.get_node_attributes(g, GRAPH_SRC_ATTRIBUTE)
     # use better graphviz layout
     pos = nx.drawing.nx_pydot.graphviz_layout(g) if dot_layout else None
     nx.draw(g, labels=labels, node_size=100, ax=ax, pos=pos)
