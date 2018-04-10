020bdc62a905ec5dd352f574a1601241e5c3165c
Jose Cambronero <jcamsan@mit.edu>
2018-04-10 17:24:58 -0400

    added memory update refinements to graph builder. Allows updating the lineno associated with references to exclude the base object, or only consider the most specific (i.e. the deepest nested reference)

--- plpy/analyze/graph_builder.py
+++ plpy/analyze/graph_builder.py
@@ -1,18 +1,31 @@
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawTextHelpFormatter
+from enum import Enum
 import networkx as nx
 import pickle
+import textwrap
 
-from .dynamic_tracer import DynamicDataTracer
+
+from .dynamic_tracer import DynamicDataTracer, get_nested_references, to_ast_node
 from .dynamic_trace_events import *
 
+class MemoryRefinementStrategy(Enum):
+    INCLUDE_ALL = 0
+    IGNORE_BASE = 1
+    MOST_SPECIFIC = 2
 
 class DynamicTraceToGraph(object):
-    def __init__(self, ignore_unknown=False):
+    def __init__(self, ignore_unknown=False, memory_refinement=0):
+        # do not construct nodes for unknown memory references
+        self.ignore_unknown = ignore_unknown
+        # only consume memory update based on policy
+        self.memory_refinement = MemoryRefinementStrategy(memory_refinement)
+        # graph with statement nodes and edges for data dependencies
+        self.graph = nx.DiGraph()
+        # counter for node identifiers
         self.counter = 0
         # note that pydot doesn't like negatives...
         self.unknown_id = self.allocate_node_id()
-        self.graph = nx.DiGraph()
-        self.ignore_unknown = ignore_unknown
+        # mappings to node identifiers
         self.lineno_to_nodeid = {}
         self.mem_loc_to_lineno = {}
         self.consuming = []
@@ -25,7 +38,7 @@ class DynamicTraceToGraph(object):
     def create_and_add_node(self, node_id, trace_event):
         self.graph.add_node(node_id)
         # set up attributes
-        attributes = ['src', 'lineno']
+        attributes = ['src', 'lineno', 'event', 'defs']
         for attr in attributes:
             self.graph.nodes[node_id][attr] = None
         if node_id == self.unknown_id:
@@ -33,6 +46,7 @@ class DynamicTraceToGraph(object):
         else:
             self.graph.nodes[node_id]['src'] = trace_event.line
             self.graph.nodes[node_id]['lineno'] = trace_event.lineno
+            self.graph.nodes[node_id]['event'] = trace_event
 
     def handle_ExecLine(self, event):
         if self.consuming:
@@ -41,7 +55,6 @@ class DynamicTraceToGraph(object):
         node_id = self.allocate_node_id()
         self.create_and_add_node(node_id, event)
         dependencies = []
-        # ignore mem locations that are unknown
         for name, ml in event.uses_mem_locs.items():
             if (not self.ignore_unknown) or ml in self.mem_loc_to_lineno:
                 ml_id = self.get_latest_node_id_for_mem_loc(name, ml)
@@ -63,12 +76,63 @@ class DynamicTraceToGraph(object):
             lineno = self.mem_loc_to_lineno[mem_loc]
             return self.lineno_to_nodeid[lineno]
 
+    @staticmethod
+    def refine_ignore_base(mem_locs_dict):
+        """
+        ignore base memory update for references to 'containers'
+        """
+        bases = set([])
+        for name in mem_locs_dict.keys():
+            ast_node_name = to_ast_node(name)
+            refs = get_nested_references(ast_node_name, exclude_first=True)
+            refs = sorted(refs, key=len)
+            if refs:
+                bases.add(refs[0])
+        return {k:v for k, v in mem_locs_dict.items() if not k in bases}
+
+    @staticmethod
+    def refine_most_specific(mem_locs_dict):
+        """
+        ignore all but the most specific memory update for references to 'containers'
+        """
+        nested_references = set([])
+        for name in mem_locs_dict.keys():
+            ast_node_name = to_ast_node(name)
+            refs = get_nested_references(ast_node_name, exclude_first=True)
+            nested_references.update(refs)
+        return {k:v for k, v in mem_locs_dict.items() if not k in nested_references}
+
+    def refine_memory_updates(self, mem_locs_dict):
+        # refine memory locations according to a specific strategy
+        # this refinement is purely syntactic
+        # doing this based on actual memory addresses isn't really feasible
+        # for example, given a data frame df, df['c1'] always returns the same
+        # id as long as unmodified, but retrieving first element,
+        # id(df['c1'][0]), repeatedly returns different value as it allocates a new
+        # np.dtype object.
+        # If instead we retrieve with df['c1'].values.item(0) we always get same id
+        # as it copies it to a Python object...
+        # all this to say: inferring related objects from memory addresses hardly
+        # seems bulletproof, so might as well just do syntactically
+        if self.memory_refinement == MemoryRefinementStrategy.INCLUDE_ALL:
+            return mem_locs_dict
+        elif self.memory_refinement == MemoryRefinementStrategy.IGNORE_BASE:
+            return self.refine_ignore_base(mem_locs_dict)
+        elif self.memory_refinement == MemoryRefinementStrategy.MOST_SPECIFIC:
+            return self.refine_most_specific(mem_locs_dict)
+        else:
+            raise Exception("Invalid memory refinement strategy: %s" % self.memory_refinement)
+
     def handle_MemoryUpdate(self, event):
         if self.consuming:
             return
-
-        for name, mem_loc in event.mem_locs.items():
+        mem_locs = dict(event.mem_locs)
+        mem_locs = self.refine_memory_updates(mem_locs)
+        for name, mem_loc in mem_locs.items():
             self.mem_loc_to_lineno[mem_loc] = event.lineno
+            # add these mem_locs to the line node that created them
+            line_node_id = self.lineno_to_nodeid[event.lineno]
+            self.graph.nodes[line_node_id]['defs'] = mem_locs
 
     def handle_EnterCall(self, event):
         self.consuming += [event]
@@ -102,7 +166,7 @@ def draw(g, dot_layout=True):
 def main(args):
     with open(args.input_path, 'rb') as f:
         tracer = pickle.load(f)
-    builder = DynamicTraceToGraph(ignore_unknown=args.ignore_unknown)
+    builder = DynamicTraceToGraph(ignore_unknown=args.ignore_unknown, memory_refinement=args.memory_refinement)
     graph = builder.run(tracer)
     with open(args.output_path, 'wb') as f:
         pickle.dump(graph, f)
@@ -114,15 +178,25 @@ def main(args):
         plt.show(block=args.block)
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Build networkx graph from tracer (with events)')
+    parser = ArgumentParser(description='Build networkx graph from tracer (with events)', formatter_class=RawTextHelpFormatter)
     parser.add_argument('input_path', type=str, help='Path to pickled tracer (with events)')
     parser.add_argument('output_path', type=str, help='Path to store pickled networkx graph')
     parser.add_argument('-i', '--ignore_unknown', action='store_true', help='Exclude unknown memory locations from graph')
+    parser.add_argument('-m', '--memory_refinement', type=int, help=
+    textwrap.dedent("""
+    0: apply all memory updates (MemoryRefinementStrategy.INCLUDE_ALL) (DEFAULT)
+    1: ignore base (MemoryRefinementStrategy.IGNORE_BASE)
+    2: ignore all but most specific (MemoryRefinementStrategy.MOST_SPECIFIC)
+    Determined syntactically
+    """),
+    default=0
+    )
     parser.add_argument('-d', '--draw', action='store_true', help='Draw graph and display')
     parser.add_argument('-b', '--block', action='store_true', help='Block when displaying graph')
     args = parser.parse_args()
+
     try:
         main(args)
-    except:
+    except Exception as err:
         import pdb
         pdb.post_mortem()
