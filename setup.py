0dd6db4bc63feacc1f1a3d25e3c3c6544562ef2c
Jose Cambronero <jcamsan@mit.edu>
2020-09-02 17:45:09 -0400

    update package versions

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup
 
-setup(name='plpy',
-      version='0.1',
-      description='Utilities for Python program analysis',
-      url='TODO',
-      author='Jose Cambronero',
-      author_email='jcamsan@mit.edu',
-      license='MIT',
-      packages=['plpy'],
-      install_requires=[
-          'astunparse==1.6',
-          'matplotlib==2.2.2',
-          'networkx==2.1',
-          'pydot==1.2.4',
-          'pandas==0.23.4'
-
-      ],
-      setup_requires=['pytest-runner'],
-      tests_require=['pytest'],
-      zip_safe=False,
-      )
+setup(
+    name='plpy',
+    version='0.1',
+    description='Utilities for Python program analysis',
+    url='TODO',
+    author='Jose Cambronero',
+    author_email='jcamsan@mit.edu',
+    license='MIT',
+    packages=['plpy'],
+    install_requires=[
+        'astunparse==1.6.3',
+        'matplotlib==2.2.2',
+        'networkx==2.1',
+        'pydot==1.2.4',
+        'pandas==0.23.4',
+        'numpy==1.14.6',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    zip_safe=False,
+)
